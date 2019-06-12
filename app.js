b7aa706d6f788e2e0baf5efc2bae04b8b7bdf0d1
Ralph Wiest <lioncrazed@gmail.com>
2019-06-12 16:44:13 -0400

    added html page serving

---
+++ app.js
@@ -0,0 +1,21 @@
+const path = require('path');
+
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+const projectPath = require('./utils/path');
+
+const app = express();
+
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.use('/admin', adminRoutes);
+app.use(shopRoutes);
+
+app.use((req, res, next) => {
+  res.status(404).sendFile(path.join(projectPath, 'views', 'not-found.html'));
+});
+
+app.listen(3000);
