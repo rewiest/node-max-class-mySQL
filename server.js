46e21f875a6ade7d0aa39f8c2b83621d44f96742
Ralph Wiest <lioncrazed@gmail.com>
2019-06-11 23:02:19 -0400

    express first assignment

--- server.js
+++ server.js
@@ -1,41 +1,15 @@
-const http = require('http');
-const fs = require('fs');
+const express = require('express');
 
-const server = http.createServer((req, res) => {
-  const url = req.url;
-  const method = req.method;
-  // check for '/' path, and if so, respond with HTML form and then return
-  if (url === '/') {
-    console.log('Path of / encountered...');
-    res.write('<html>');
-    res.write('<head><title>Enter Message</title></head>');
-    res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>');
-    res.write('</html>');
-    return res.end();
-  }
-  // check for '/message' path, and if so, read in form data and write out to file
-  if (url === '/message' && method === 'POST') {
-    console.log('Path of /message encountered...');
-    const body = [];
-    // async code - register listener to look for data chunks and run code block when triggered
-    req.on('data', (chunk) => {
-      console.log(chunk);
-      body.push(chunk);
-    });
-    // async code - register listener to look for end and then return, run code block when triggered 
-    return req.on('end', () => {
-      const parsedBody = Buffer.concat(body).toString();
-      console.log(parsedBody);
-      const message = parsedBody.split('=')[1];
-      // async code - initiate file write, run code block when done that includes setting response
-      fs.writeFile('message.txt', message, (err) => {
-        console.log('Message written to file.')
-        res.statusCode = 302;
-        res.setHeader('Location', '/');
-        return res.end(); 
-      });
-    })
-  }
+const app = express();
+
+app.use('/add-product', (req, res, next) => {
+  console.log('In add-product path...');
+  res.send('<h1>Add Product Page</h1>');
 });
 
-server.listen(3000);
+app.use('/', (req, res, next) => {
+  console.log('In home page...');
+  res.send('<h1>Home Page</h1>');
+});
+
+app.listen(3000);
