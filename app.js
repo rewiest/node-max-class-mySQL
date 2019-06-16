5c3de7c24bc1dd266e891a9fe0158cd1a5ec41d0
Ralph Wiest <lioncrazed@gmail.com>
2019-06-16 16:22:52 -0400

    add ejs templating engine

--- app.js
+++ app.js
@@ -3,21 +3,23 @@ const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
 
-const adminRoutes = require('./routes/admin');
+const adminData = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
-const projectPath = require('./utils/path');
 
 const app = express();
+
+app.set('view engine', 'ejs');
+app.set('views', 'views');
 
 app.use(bodyParser.urlencoded({extended: false}));
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/admin', adminRoutes);
+app.use('/admin', adminData.routes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-  res.status(404).sendFile(path.join(projectPath, 'views', 'not-found.html'));
+  res.status(404).render('not-found', {pageTitle: 'Page Not Found', path: ''});
 });
 
 app.listen(3000);
--- routes/shop.js
+++ routes/shop.js
@@ -2,10 +2,18 @@ const path = require('path');
 
 const express = require('express');
 
+const rootDir = require('../utils/path');
+const adminData = require('./admin');
+
 const router = express.Router();
 
 router.get('/', (req, res, next) => {
-  res.sendFile(path.join(__dirname, '..', 'views', 'shop.html'));
+  const products = adminData.products;
+  res.render('shop', {
+    pageTitle: 'Shop',
+    prods: products,
+    path: '/'
+  });
 });
 
 module.exports = router;
