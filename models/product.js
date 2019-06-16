fc3a748ad259aa41df85eee158bd0d282943340d
Ralph Wiest <lioncrazed@gmail.com>
2019-06-16 19:49:50 -0400

    added writing and reading data from file

--- models/product.js
+++ models/product.js
@@ -1,4 +1,21 @@
-const products = [];
+const fs = require('fs');
+const path = require('path');
+
+const productsPathAndFile = path.join(
+  path.dirname(process.mainModule.filename),
+  'data',
+  'products.json'
+);
+
+const getProductsFromFile = (cb) => {
+  fs.readFile(productsPathAndFile, (err, fileContent) => {
+    if (err) {
+      cb([]);
+    } else {
+      cb(JSON.parse(fileContent));
+    }
+  });
+};
 
 module.exports = class Product {
   constructor(title) {
@@ -6,10 +23,16 @@ module.exports = class Product {
   }
 
   save() {
-    products.push(this);
+    getProductsFromFile( products => {
+      products.push(this);
+      fs.writeFile(productsPathAndFile, JSON.stringify(products), (err) => {
+        console.log(err);
+      });
+    });
   }
 
-  static fetchAll() {
-    return products;
+  static fetchAll(cb) {
+    getProductsFromFile(cb);
   }
-}
+
+};
