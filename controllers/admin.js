5b97bc2ab197c9d280683607c4e662a140cd97ab
Ralph Wiest <lioncrazed@gmail.com>
2019-06-16 22:56:11 -0400

    start enhancing shop app

--- controllers/admin.js
+++ controllers/admin.js
@@ -1,7 +1,7 @@
 const Product = require('../models/product');
 
 exports.getAddProduct = (req, res, next) => {
-  res.render('add-product', {
+  res.render('admin/add-product', {
     pageTitle: 'Add Product',
     path: '/admin/add-product'
   });
@@ -15,10 +15,10 @@ exports.postAddProduct = (req, res, next) => {
 
 exports.getProducts = (req, res, next) => {
   Product.fetchAll(products => {
-    res.render('shop', {
-      pageTitle: 'Shop',
+    res.render('admin/products', {
+      pageTitle: 'Admin Products',
       prods: products,
-      path: '/'
+      path: '/admin/products'
     });
   });
 }
